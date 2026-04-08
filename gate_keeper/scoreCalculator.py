8eb220ddc2e8a26537a2986cad4a64add55ca8ae
Andy-cccc <229692753@qq.com>
2026-04-08 13:44:07 +1000

    update

--- gate_keeper/scoreCalculator.py
+++ gate_keeper/scoreCalculator.py
@@ -1,9 +1,12 @@
 
 
 def cal_integrity(doc):
-    
+    #Fidelity-to-Context 
     result = "$The integrity score is ?$"
     return result
 
-def cal_truthfulness():
-    return 0    
+def cal_truthfulness(doc):
+    #Fidelity-to-Reality
+    result=""
+    
+    return result    
